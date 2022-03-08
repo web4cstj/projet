12f7c0c6a8a59b087b8d3db1317ab4234ec4a8f3
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2022-03-07 22:45:46 -0500

    Ajout de détails

--- public/index.php
+++ public/index.php
@@ -7,9 +7,16 @@ include_once "../vendor/autoload.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="interface">
+        <header><h1>Document</h1></header>
+        <div class="body">
+
+        </div>
+        <footer>&copy;</footer>
+    </div>
 </body>
 </html>
