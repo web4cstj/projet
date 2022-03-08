06366f2ca50f52527e39528b8c6f93f5f170cf70
Martin Boudreau <mboudrea@cstj.qc.ca>
2022-03-08 11:42:37 -0500

    Ajusté le PSR-0

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ include_once "../vendor/autoload.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Document</title>
 </head>
